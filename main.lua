baaf5d0316d418e66def5400981cde3ed1da43d4
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2023-07-01 15:06:02 +0200

    Jump physics done

--- main.lua
+++ main.lua
@@ -1,15 +1,18 @@
 love.load = function()
+    love.window.setTitle("Moon Invasion Defense")
     player = {
         x = 100,
         y = 100,
-        radius = 50,
+        radius = 40,
         physics = {
             velocity = {
                 x = 0,
                 y = 0
             },
-            gravity = 0.1,
-            speed = 5
+            gravity = 0.4,
+            speed = 5,
+            jump_force = 15,
+            grounded = false,
         }
     }
     window_width, window_height = love.graphics.getDimensions()
@@ -19,21 +22,24 @@ love.update = function()
     player.physics.velocity.x = 0
 
     if player.y < window_height - player.radius then
-        player.physics.velocity.y = player.physics.speed
+        player.physics.velocity.y = player.physics.velocity.y + player.physics.gravity
+        player.physics.grounded = false
     else
         player.physics.velocity.y = 0
+        player.physics.grounded = true
     end
 
-    if love.keyboard.isDown('a') and player.x > 50 then
-        player.physics.velocity.x = -player.physics.speed
+    if love.keyboard.isDown('space') and player.y > player.radius and player.physics.grounded then
+        player.physics.velocity.y = -player.physics.jump_force
     elseif love.keyboard.isDown('d') and player.x < window_width - player.radius then
         player.physics.velocity.x = player.physics.speed
-    elseif love.keyboard.isDown('w') and player.y > 50 then
-        player.physics.velocity.y = -player.physics.speed
+    elseif love.keyboard.isDown('a') and player.x > player.radius then
+        player.physics.velocity.x = -player.physics.speed
     end
-
     player.x = player.x + player.physics.velocity.x
     player.y = player.y + player.physics.velocity.y
+
+    fps = love.timer.getFPS()
 end
 
 love.draw = function()
@@ -43,4 +49,6 @@ love.draw = function()
         player.y,
         player.radius
     )
+    love.graphics.print(tostring(player.physics.grounded), 0, 15)
+    love.graphics.print(fps)
 end
